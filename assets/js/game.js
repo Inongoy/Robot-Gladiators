46ad68ba9f0b5aa081c08f6cc0ec778141db540d
Inongoy <Ingoy247@icloud.com>
2022-09-09 06:04:58 -0400

    added code to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,35 @@
+// this creates a function named "fight"//
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+
+var playerName = window.prompt("what is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+
+console.log(playerName);
+
+console.log("This log is a string, good for leaving yourself a message");
+
+// This will do math and log 20
+
+console.log(10 + 10);
+
+// What is this?
+
+console.log("Our robot's name is " + playerName);
+
+var playerName = "jasper";
+
+// jasper is ready for battle!
+
+console.log("jasper" + "is ready for battle!");
+
+// "jasper is ready for battle!"
+
+console.log(playerName + "is ready for battle!");
+
+// "your robot, jasper, has won!"
+
+console.log("your robot ," + playerName + ", has won!");
